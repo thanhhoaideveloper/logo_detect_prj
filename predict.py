82ae59f4acbb2cfb07da73180ebbcd5a8b254bcc
tuanphan <tuanpa@mor.com.vn>
2024-02-23 10:04:17 +0700

    Update

--- predict.py
+++ predict.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
 
-model = YOLO("./model/bestV5.pt")
+model = YOLO("./model/bestV2.pt")
 
 predict_path = "assets/target"
 test_path = "assets/test"
-result = model(test_path, save = True, project=predict_path, conf=0.5)
+results = model(test_path, save = True, project=predict_path, conf=0.5, show_conf=True, save_conf=True)
